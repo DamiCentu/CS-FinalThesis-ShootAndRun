22c1ea06333c28df3ba74bf8c8dba622dad841e1
Flor <flopy1706@hotmail.com>
2018-09-14 01:10:43 -0300

    Empiezo a crear un nuevo enemigo

--- Assets/Scripts/Managers/EnemiesManager.cs
+++ Assets/Scripts/Managers/EnemiesManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,18 +16,21 @@ public class EnemiesManager : MonoBehaviour {
     public GameObject miniBossPrefab;
     public GameObject bossPrefab;
     public GameObject cubeEnemyPrefab;
+    public GameObject misilEnemyPrefab;
 
     Pool<NormalEnemyBehaviour> _poolOfNormalEnemy;
     Pool<ChargerEnemyBehaviour> _poolOfChargerEnemy;
     Pool<EnemyTurretBehaviour> _poolOfTurretEnemy;
     Pool<PowerUpChaserEnemy> _poolOfChaserEnemy;
     Pool<CubeEnemyBehaviour> _poolOfCubeEnemy;
+    Pool<MisilEnemy> _poolOfMisilEnemy;
 
     GameObject _normalContainer;
     GameObject _turretContainer;
     GameObject _chargerContainer;
     GameObject _chaserContainer;
     GameObject _cubeContainer;
+    GameObject _misilContainer;
 
     void Awake() {
         Debug.Assert(FindObjectsOfType<EnemiesManager>().Length == 1);
@@ -38,13 +42,14 @@ public class EnemiesManager : MonoBehaviour {
         _chargerContainer = new GameObject("ChargerEnemyContainer");
         _chaserContainer = new GameObject("ChaserEnemyContainer");
         _cubeContainer = new GameObject("CubeEnemyContainer");
+        _misilContainer = new GameObject("MisilEnemyCointener");
 
         _poolOfNormalEnemy = new Pool<NormalEnemyBehaviour>(30, NormalFactoryMethod, null,null,true);
         _poolOfChargerEnemy = new Pool<ChargerEnemyBehaviour>(10, ChargerFactoryMethod, null, null, true);
         _poolOfTurretEnemy = new Pool<EnemyTurretBehaviour>(10, TurretFactoryMethod, null, null, true);
         _poolOfChaserEnemy = new Pool<PowerUpChaserEnemy>(5, ChaserFactoryMethod, null, null, true);
         _poolOfCubeEnemy = new Pool<CubeEnemyBehaviour>(5, CubeFactoryMethod, null, null, true);
-
+        _poolOfMisilEnemy = new Pool<MisilEnemy>(5, MisilFactoryMethod, null, null, true);
     }
 
     #region NORMAL EnemyBehaviour methods
@@ -127,6 +132,26 @@ public class EnemiesManager : MonoBehaviour {
     }
     #endregion
 
+    #region Misil EnemyBehaviour methods
+    public MisilEnemy MisilFactoryMethod()
+    {
+        var a = Instantiate(misilEnemyPrefab.GetComponent<MisilEnemy>(), _misilContainer.transform);
+        a.gameObject.SetActive(false);
+        return a;
+    }
+
+    public MisilEnemy GiveMeMisilEnemy()
+    {
+        return _poolOfMisilEnemy.GetObjectFromPool();
+    }
+
+    public void ReturnMisilEnemyToPool(MisilEnemy enemy)
+    {
+        _poolOfMisilEnemy.DisablePoolObject(enemy);
+    }
+    #endregion
+
+
     public enum TypeOfEnemy {
         Normal,
         Charger,
@@ -134,6 +159,7 @@ public class EnemiesManager : MonoBehaviour {
         TurretLaser,
         PowerUpChaser,
         Cube,
-        Boss
+        Boss,
+        MisilEnemy
     } 
 }
