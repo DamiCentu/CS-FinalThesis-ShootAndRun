869d2f352b278d00dbdd23fa794c04c72bafa6ac
Damian Centurion <damicentu@gmail.com>
2018-09-15 15:40:45 -0300

    Añado el feedback blanco ahora funciona bien, corrijo algunos errores

--- Assets/Scripts/Characters/Enemies/AbstractEnemy.cs
+++ Assets/Scripts/Characters/Enemies/AbstractEnemy.cs
@@ -4,19 +4,7 @@ using UnityEngine;
 
 public abstract class AbstractEnemy : MonoBehaviour, IOffScreen {
     protected SectionManager.WaveNumber _actualWave;
-    protected SectionNode _actualSectionNode;
-    //public float whiteTimeOnHit = .2f;
-    //public Material mainMat;
-
-    //public Material whiteMat;
-
-    //Material mainMat;
-    //WaitForSeconds _waitWhite;
-    //SkinnedMeshRenderer[] _skinnedRnds;
-    //MeshRenderer[] _meshRnds;
-
-    //bool _hitRoutineRunning = false; 
-    //IEnumerator _onHitRoutine;
+    protected SectionNode _actualSectionNode; 
 
     OnHitWhiteFeedback _onHitWhiteFeedback;
 
@@ -56,110 +44,16 @@ public abstract class AbstractEnemy : MonoBehaviour, IOffScreen {
     }
 
     //settea el ponerse blanco cuando le pegan
-    public AbstractEnemy SetTimeAndRenderer() {
-        //_waitWhite = new WaitForSeconds(whiteTimeOnHit);
+    public AbstractEnemy SetTimeAndRenderer() { 
         if(_onHitWhiteFeedback == null) {
             _onHitWhiteFeedback = GetComponent<OnHitWhiteFeedback>();
         }
 
         _onHitWhiteFeedback.SetFeedback();
-
-        //if(_skinnedRnds == null)
-        //    _skinnedRnds = GetComponentsInChildren<SkinnedMeshRenderer>();
-
-        //if (_meshRnds == null)
-        //    _meshRnds = GetComponentsInChildren<MeshRenderer>();
-
-        //if (_skinnedRnds.Length > 0) { 
-        //    if(mainMat == null) { 
-        //        mainMat = _skinnedRnds[0].material;
-        //    }
-        //    foreach (var mt in _skinnedRnds) { 
-        //        mt.material = mainMat;
-        //    }
-        //}
-
-        //if (_meshRnds.Length > 0) {
-        //    if (mainMat == null)
-        //        mainMat = _meshRnds[0].material;
-        //    foreach (var mt in _meshRnds) 
-        //        mt.material = mainMat;
-        //}
-
         return this;
     } 
 
     protected void AbstractOnHitWhiteAction() {
-        //if(_hitRoutineRunning) {
-        //    StopCoroutine(_onHitRoutine);
-        //    ResetMat();
-        //}
-
-        //_onHitRoutine = OnHitRoutine();
-
-        //StartCoroutine(_onHitRoutine); 
-
         _onHitWhiteFeedback.OnHit();
     }
-
-    //protected void ResetMat() {
-    //    if (_skinnedRnds != null) { 
-    //        foreach (var mt in _skinnedRnds) {
-    //            if (mt == null)
-    //                continue;
-    //            mt.material = mainMat;
-    //        }
-    //    }
-
-    //    if (_meshRnds != null) { 
-    //        foreach (var mt in _meshRnds) {
-    //            if (mt == null)
-    //                continue;
-    //            mt.material = mainMat;
-    //        }
-    //    }
-    //}
-
-    //void OnDisable() {
-    //    ResetMat();
-    //}
-
-    //IEnumerator OnHitRoutine() {
-    //    _hitRoutineRunning = true;
-
-    //    if (_skinnedRnds != null) { 
-    //        foreach (var mt in _skinnedRnds) {
-    //            if (mt == null)
-    //                continue;
-    //            mt.material = whiteMat;
-    //        }
-    //    }
-
-    //    if (_meshRnds != null) { 
-    //        foreach (var mt in _meshRnds) {
-    //            if (mt == null)
-    //                continue;
-    //            mt.material = whiteMat;
-    //        }
-    //    }
-
-    //    yield return _waitWhite;
-
-    //    if (_skinnedRnds != null) { 
-    //        foreach (var mt in _skinnedRnds) {
-    //            if (mt == null)
-    //                continue;
-    //            mt.material = mainMat;
-    //        }
-    //    }
-
-    //    if (_meshRnds != null) { 
-    //        foreach (var mt in _meshRnds) {
-    //            if (mt == null)
-    //                continue;
-    //            mt.material = mainMat;
-    //        }
-    //    }
-    //    _hitRoutineRunning = false;
-    //}
 }
