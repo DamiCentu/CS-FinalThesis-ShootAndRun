74b9499105cd4b6a4004baac36f41557e95e9fd1
Damian Centurion <damicentu@gmail.com>
2018-09-16 23:14:40 -0300

    Revert "Revert "Merge branch 'master' of https://github.com/DamiCentu/Shoot-and-Run""
    This reverts commit 540ab80f57d4cbea188c1f6d765b559ec1e52e99.

---
+++ Assets/Scripts/Characters/Enemies/OnHitWhiteFeedback.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHitWhiteFeedback : MonoBehaviour {
+
+    public float whiteTimeOnHit = 0.1f; 
+    public Material whiteMat;
+
+    Material mainMat; 
+    SkinnedMeshRenderer[] _skinnedRnds;
+    MeshRenderer[] _meshRnds;
+
+    bool _hitted;
+    float _time;
+
+    void Start () {
+		
+	}
+
+	void Update () {
+        if (_hitted) {
+            OnHitCheck();
+            _time += Time.deltaTime;
+        }
+	}
+
+    //esto se llama cuando el enemigo es golpeado por algo
+    public void OnHit() {
+        _time = 0;
+        _hitted = true;
+        _alreadyChangedMat = false;
+    }
+
+    //esto se llama cuando el enemigo es seteado en la escena
+    public void SetFeedback() {
+        if(_skinnedRnds == null) { 
+            _skinnedRnds = GetComponentsInChildren<SkinnedMeshRenderer>();
+        }
+
+        if (_meshRnds == null) { 
+            _meshRnds = GetComponentsInChildren<MeshRenderer>();
+        }
+
+        if (_skinnedRnds.Length > 0) { 
+            if(mainMat == null) { 
+                mainMat = _skinnedRnds[0].material;
+            }
+            foreach (var mt in _skinnedRnds) { 
+                mt.material = mainMat;
+            }
+        }
+
+        if (_meshRnds.Length > 0) {
+            if (mainMat == null) { 
+                mainMat = _meshRnds[0].material;
+            }
+            foreach (var mt in _meshRnds) { 
+                mt.material = mainMat;
+            }
+        }
+
+        ResetMat();
+    } 
+
+    //esto se deberia llamar para volver a poner el material original 
+    void ResetMat() {
+        _hitted = false;
+        _alreadyChangedMat = false;
+        _time = 0;
+
+        if (_skinnedRnds.Length > 0) { 
+            foreach (var mt in _skinnedRnds) {
+                if (mt == null)
+                    continue;
+                mt.material = mainMat;
+            }
+        }
+
+        if (_meshRnds.Length > 0) { 
+            foreach (var mt in _meshRnds) {
+                if (mt == null)
+                    continue;
+                mt.material = mainMat;
+            }
+        }
+    }
+
+    bool _alreadyChangedMat;
+
+    //este metodo chequea el tiempo transcurrido para volver a poner el material correspondiente
+    void OnHitCheck() {  
+        if(_time < whiteTimeOnHit) {
+            if (_alreadyChangedMat) {
+                return;
+            }
+
+            if (_skinnedRnds.Length > 0) { 
+                foreach (var mt in _skinnedRnds) {
+                    if (mt == null)
+                        continue;
+                    mt.material = whiteMat;
+                }
+            }
+
+            if (_meshRnds.Length > 0) { 
+                foreach (var mt in _meshRnds) {
+                    if (mt == null)
+                        continue;
+                    mt.material = whiteMat;
+                }
+            }
+            _alreadyChangedMat = true;
+        }
+        else { //---------------------------------------------
+            ResetMat();
+            _alreadyChangedMat = false;
+            _hitted = false;
+            _time = 0;
+        }
+    }
+}
