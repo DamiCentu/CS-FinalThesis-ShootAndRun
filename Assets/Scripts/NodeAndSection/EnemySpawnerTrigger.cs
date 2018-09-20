e6ef9ce02657ce4712d107975867ffbd08522a81
Damian Centurion <damicentu@gmail.com>
2018-09-19 22:13:49 -0300

    añado comportamiento para seguir mañana y lo integro!

--- Assets/Scripts/NodeAndSection/EnemySpawnerTrigger.cs
+++ Assets/Scripts/NodeAndSection/EnemySpawnerTrigger.cs
@@ -6,23 +6,49 @@ public class EnemySpawnerTrigger : MonoBehaviour {
     public SectionNode nodeOfTriggering;
     public bool spawnWaves = false;
     public GameObject allTriggerSpawnersContainer;
+    public TypeOfTrigger typeOfTrigger;
 
     EnemySpawner[] _allTriggerSpawners;
 
+    public enum TypeOfTrigger {
+        SpawnWaves,
+        SpawnEnemiesPreSetted,
+        SpawnEnemiesInPortal
+    }
+
     void Start() {
-        if (!spawnWaves)
+        if (typeOfTrigger == TypeOfTrigger.SpawnEnemiesPreSetted) {
+         //   if (!spawnWaves) { 
             _allTriggerSpawners = allTriggerSpawnersContainer.GetComponentsInChildren<EnemySpawner>();
+        }
     }
 
     void OnTriggerEnter(Collider c) {
         if(c.gameObject.layer == 8) {
-            if (nodeOfTriggering != null) {
-                if(spawnWaves)
+
+            switch (typeOfTrigger) {
+
+                case TypeOfTrigger.SpawnWaves:
                     nodeOfTriggering.StartNodeRoutine();
-                else
+                    break;
+                case TypeOfTrigger.SpawnEnemiesPreSetted:
                     nodeOfTriggering.TriggerSpawn(_allTriggerSpawners);
-                gameObject.SetActive(false);
+                    break;
+                case TypeOfTrigger.SpawnEnemiesInPortal:
+                    break;
+
             }
+
+            gameObject.SetActive(false);
+
+            //if (spawnWaves) { 
+            //    nodeOfTriggering.StartNodeRoutine();
+            //}
+            //else { 
+            //    nodeOfTriggering.TriggerSpawn(_allTriggerSpawners);
+            //}
+            //gameObject.SetActive(false);
+            
         }
     }
 }
