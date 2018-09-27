dc8597d0cb2f015bd8206afbc235eafa252aa280
Flor <flopy1706@hotmail.com>
2018-09-27 14:56:49 -0300

    el fuego hace daño si te chocas

---
+++ Assets/DamageTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTrigger : MonoBehaviour {
+    public LayerMask layerThatDontAffect;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (layerThatDontAffect != (layerThatDontAffect | (1 << other.gameObject.layer)))
+        {
+            Player p = other.gameObject.GetComponent<Player>();
+            if (p != null)
+            {
+                p.OnHit(1);
+            }
+            print("me choque");
+            EventManager.instance.ExecuteEvent(Constants.CHARGER_CRUSH);
+
+        }
+    }
+}
+
