3be693df7e5a985887c0845751358871773727f9
Flor <flopy1706@hotmail.com>
2018-09-28 13:07:13 -0300

    arreglo lo roto

---
+++ Assets/ReflexBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflexBullet : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.forward = new Vector3(-other.transform.forward.x, other.transform.forward.z, -other.transform.forward.z);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        Quaternion rot = other.transform.rotation;
+        rot.eulerAngles = new Vector3(rot.eulerAngles.x, -rot.eulerAngles.y, rot.eulerAngles.z);
+        other.transform.rotation = rot;
+    }
+}
