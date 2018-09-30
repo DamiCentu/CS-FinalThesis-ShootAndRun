9ab00a9e8471048d3b003735762c04ec4e17d1f7
Flor <flopy1706@hotmail.com>
2018-09-30 17:42:11 -0300

    empiezo ulti nueva

--- Assets/Scripts/Characters/Player/Minion.cs
+++ Assets/Scripts/Characters/Player/Minion.cs
@@ -1,16 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Minion : MonoBehaviour {
+    private Vector3 _target;
+    public float radius;
+    public LayerMask hittableLayers;
+    public Transform shootPos;
+    public float minTimeTiShoot;
+    Timer _timer;
+    // Use this for initialization
+    void Start () {
+         _timer = new Timer(minTimeTiShoot, Shoot);
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Shoot()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius, hittableLayers);
+        int i = 0;
+        float minDis = 10000;
+        while (i < hitColliders.Length)
+        {
+            float dis = Vector3.Distance(transform.position, hitColliders[i].transform.position);
+            if (dis < minDis)
+            {
+                minDis = dis;
+                this.transform.LookAt(hitColliders[i].transform.position);
+            }
+            i++;
+        }
+        if (minDis < 10000)
+        { // si encontro a algun enemigo dispara
+            var s = EnemyBulletManager.instance.giveMeEnemyBullet();
+            s.SetPos(shootPos.position).SetDir(Quaternion.LookRotation(transform.forward)).gameObject.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (_timer.CheckAndRun()) {
+            _timer.Reset();
+        }
+        
+    }
+
 }
