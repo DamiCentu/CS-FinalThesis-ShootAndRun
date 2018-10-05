2e684e4f72164d0660c253c10e0f4fb456f6609f
Flor <flopy1706@hotmail.com>
2018-10-05 10:57:38 -0300

    cambio unas cositas

---
+++ Assets/myVideoPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class myVideoPlayer : MonoBehaviour {
+    public RawImage image;
+    public VideoPlayer vp;
+    public AudioSource audio;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(PlayVideo());
+	}
+
+    IEnumerator PlayVideo() {
+        vp.Prepare();
+        WaitForSeconds wait = new WaitForSeconds(1);
+        while (!vp.isPrepared) {
+            yield return wait;
+            break;
+        }
+        image.texture = vp.texture;
+        vp.Play();
+    }
+}
