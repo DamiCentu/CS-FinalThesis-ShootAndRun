9555cdb9537e19ad2797ddd4620a30cc79a2d042
Flor <flopy1706@hotmail.com>
2018-10-12 12:17:45 -0300

    stage 3 en el juego

---
+++ Assets/FixPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixPosition : MonoBehaviour {
+    public float x;
+    public float y;
+    public float z;
+
+	// Use this for initialization
+	void Awake () {
+        this.transform.localPosition= new Vector3(x,y,z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
