3f95fac3237ad28c417e52013fc50dafceb04293
Flor <flopy1706@hotmail.com>
2018-10-14 23:44:21 -0300

    herramienta para poner cubos

---
+++ Assets/CreadorMapaModular.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CreadorMapaModular : MonoBehaviour {
+    public GameObject prefab;
+    public bool instance=false;
+    public int x;
+    public int z;
+    public Transform initialPos;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (instance == true)
+        {
+            instance = false;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < z; j++)
+                {
+
+                    Instantiate(prefab,new Vector3(i,0,j)+ initialPos.position, initialPos.transform.rotation);
+                }
+            }
+
+        }
+	}
+}
