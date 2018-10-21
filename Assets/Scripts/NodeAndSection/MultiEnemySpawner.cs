0a516d59ccc512a0747159a3444b7caa45328730
Damian Centurion Notebook <damicentu@gmail.com>
2018-10-21 10:26:01 -0300

    arreglo el bug de la seccion 3.5,

--- Assets/Scripts/NodeAndSection/MultiEnemySpawner.cs
+++ Assets/Scripts/NodeAndSection/MultiEnemySpawner.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class MultiEnemySpawner : MonoBehaviour {
 
     public int quantityOfEnemies = 10; 
+    [Header("Deprecated")]
     public GameObject[] triggers;
+    [Header("---------")]
+    public int quantityOfTriggersThatAffectThisSpawner;
     public float offsetY = 1f;
 
     public Vector3 GetPositionWithOffset { get { return Utility.SetYInVector3(transform.position, offsetY); } }
-    public int GetRespectiveQuantityOfEnemyPerTrigger { get { return quantityOfEnemies / triggers.Length; } }
+    public int GetRespectiveQuantityOfEnemyPerTrigger { get { return quantityOfEnemies / quantityOfTriggersThatAffectThisSpawner; } }
 }
