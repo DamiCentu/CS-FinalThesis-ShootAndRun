65eb5ef68b244b1e6ae388dc44a66d771c3d89c2
Damian Centurion <damicentu@gmail.com>
2018-11-04 17:48:21 -0300

    quedo hermoso divino

---
+++ Assets/Scripts/NodeAndSection/ReactivateSomethingTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactivateSomethingTrigger : MonoBehaviour {
+
+    public GameObject[] gameobjectsToReactivate;
+
+    private void OnTriggerEnter(Collider c) {
+        if(c.gameObject.layer == 8) { // player
+            foreach (var go in gameobjectsToReactivate) { 
+                go.SetActive(true);
+            }
+        }
+    }
+}
