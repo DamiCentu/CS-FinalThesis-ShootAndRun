f036e7fba2966046465dc8abd07a51fb115ecf8e
Flor <flopy1706@hotmail.com>
2018-11-30 03:31:36 -0300

    agrego la muerte de los misiles

--- Assets/Scripts/Characters/Enemies/Boss/FollowBullets.cs
+++ Assets/Scripts/Characters/Enemies/Boss/FollowBullets.cs
@@ -8,7 +8,7 @@ public class FollowBullets : EnemyBulletBehaviour
     public float followIntensity=0.2f;
     public float lifeTime = 3;
     public float timer;
-
+    public string DestroyParticleName = "muerte misil";
     public void Awake()
     {
         timer = lifeTime;
@@ -33,7 +33,19 @@ public class FollowBullets : EnemyBulletBehaviour
         this.transform.forward = new Vector3(this.transform.forward.x, 0f, this.transform.forward.z);
         //  this.transform.LookAt(player);
         if (timer <= 0) {
+
+            var lala= Resources.Load(DestroyParticleName);
+            var DestroyParticle2 = Instantiate((GameObject)Resources.Load(DestroyParticleName), this.transform.position, this.transform.rotation).GetComponent<ParticleSystem>();
+            DestroyParticle2.transform.position = this.transform.position;
+            DestroyParticle2.gameObject.SetActive(true);
+            DestroyParticle2.Play();
+            //    GameObject g=Instantiate(DecayMark, new Vector3(mark.transform.position.x, -0.43f, mark.transform.position.z), mark.transform.rotation);
+            //Destroy(g, 2);
+
             Destroy(this.gameObject);
+            Destroy(DestroyParticle2.gameObject, 2);
+
+
         }
     }
 
