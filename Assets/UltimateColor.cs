f3045496c5d45512a300243213919cd63492c604
Florencia Montero <f.montero@agroturn.nl>
2020-12-19 15:24:33 +0100

    arreglo la barra de la vida de lso jefes

---
+++ Assets/UltimateColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UltimateColor : MonoBehaviour {
+    Image image;
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (image.fillAmount == 1)
+        {
+            image.color = Color.white;
+        }
+        else {
+            image.color = Color.gray;
+        }
+	}
+}
