00a0535e05e603ecfd7121b43681e3dcfa0f88ed
Florencia Montero <f.montero@agroturn.nl>
2020-12-20 23:05:23 +0100

    avanzo con el menu

---
+++ Assets/Achivements/AchivementsMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchivementsMenu : MonoBehaviour {
+
+    AchivementManager achivements;
+
+    public Text title;
+    public Text description;
+
+    public Text nO_DEATH;
+    public Text mINI_BOSS_DEFEAT;
+    public Text dASH;
+    public Text lVL_2;
+    public Text lVL_1;
+    public Text rECOVER;
+    public Text almost;
+    public Text enemies100;
+
+
+
+    void Start () {
+        achivements = GetComponent<AchivementManager>();
+	}
+
+    public void NO_DEATH() {
+
+    }
+    public void MINI_BOSS_DEFEAT()
+    {
+
+    }
+    public void DASH()
+    {
+
+    }
+    public void LVL_2()
+    {
+
+    }
+    public void LVL_1()
+    {
+
+    }
+    public void RECOVER()
+    {
+
+    }
+    public void Almost()
+    {
+
+    }
+
+
+    public void Enemies100()
+    {
+
+    }
+}
